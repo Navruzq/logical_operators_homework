75c65fb2ddb5338bb1a7297052199a4a691fd422
unknown <qarshiyevnavruz7777@gmil.com>
2022-10-19 16:17:12 +0500

    12 v

--- logic12.py
+++ logic12.py
@@ -1,3 +1,6 @@
+from operator import truediv
+
+
 def main(a):
     """
     Given a two-digit integer a,  check the following statement "All digits of the number are the same".
@@ -6,4 +9,12 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    x1=a%10
+    x2=a//10
+    if x1==x2:
+        s=True
+    else:
+        s=False
+    return s
+print(main(22))
+    
