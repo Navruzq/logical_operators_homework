5305470e0c475093d8ddd0fcd193434231cda978
unknown <qarshiyevnavruz7777@gmil.com>
2022-10-19 19:54:41 +0500

    16 v

--- logic16.py
+++ logic16.py
@@ -1,3 +1,6 @@
+from re import S
+
+
 def main(a):
     """
     Given integer a,  check the following statement "The integer is a five-digit number".
@@ -6,4 +9,11 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    
+    x1=a/10000
+    if x1>=1 and x1<10:
+        s=True
+    else:
+        s=False
+    return s
+print(main(15234))
