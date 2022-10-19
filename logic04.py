16e9d84bc23f5f7ae1be5f72dfbbbd9965305a5c
unknown <qarshiyevnavruz7777@gmil.com>
2022-10-19 10:21:09 +0500

    4 v

--- logic04.py
+++ logic04.py
@@ -1,3 +1,6 @@
+from re import S
+
+
 def main(a,b):
     """
     Given two integers a, b,  check the following statement "Each of the numbers 'a' and 'b' is even".
@@ -7,4 +10,10 @@ def main(a,b):
     Returns:
         bool: answer
     """
-    return
+    s=0
+    if a%2==0 and b%2==0:
+        s="True"
+    else:
+        s="False"
+    return s
+print(main(2,4))
