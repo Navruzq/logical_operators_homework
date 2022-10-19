ad502fea2ed82dabfeb2257f0054496e4aed2d54
unknown <qarshiyevnavruz7777@gmil.com>
2022-10-19 20:15:51 +0500

    18 v

--- logic18.py
+++ logic18.py
@@ -5,4 +5,15 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    
+    x1=a%10
+    x2=a%100//10
+    x3=a%1000//100
+    x4=a%10000//1000
+    x5=a//10000
+    if x1>x2 and x2>x3 and x3>x4 and x4>x5:
+        s=True
+    else:
+        s=False
+    return s 
+print(main(12345))
